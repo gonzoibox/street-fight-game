02d4717ba1c19699052832e851300f2d39aed2aa
gonzoibox <gonzoibox@gmail.com>
2021-01-17 16:48:20 +0200

    onRejected handler

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const API_URL =
-  "https://api.github.com/repos/sahanr/street-fighter/contents/fighters.json";
+  "https://api.git--hub.com/repos/sahanr/street-fighter/contents/fighters.json";
 
 const rootElement = document.getElementById("root");
 rootElement.innerText = "Loading...";
 
 fetch(API_URL)
-  .then((response) => response.json())
+  .then(
+    (response) => {
+      return response.json();
+    },
+    (error) => {
+      console.warn(error);
+    }
+  )
   .then((file) => {
     const fighters = JSON.parse(atob(file.content));
     const names = fighters.map((it) => it.name).join("\n");
