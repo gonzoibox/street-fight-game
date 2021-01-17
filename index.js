04e4aa209604af92223d3ba2ea575faf6d429cad
gonzoibox <gonzoibox@gmail.com>
2021-01-17 16:53:12 +0200

    catch method

--- index.js
+++ index.js
@@ -5,16 +5,15 @@ const rootElement = document.getElementById("root");
 rootElement.innerText = "Loading...";
 
 fetch(API_URL)
-  .then(
-    (response) => {
-      return response.json();
-    },
-    (error) => {
-      console.warn(error);
-    }
-  )
+  .then((response) => {
+    response.json();
+  })
   .then((file) => {
     const fighters = JSON.parse(atob(file.content));
     const names = fighters.map((it) => it.name).join("\n");
     rootElement.innerText = names;
+  })
+  .catch((error) => {
+    console.warn(error);
+    root.innerText = "Failed to load data";
   });
