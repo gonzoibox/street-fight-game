05a97dff9bdfd96ab0f268f9f2b56dcfaa2d54e8
gonzoibox <gonzoibox@gmail.com>
2021-01-17 16:29:38 +0200

    Display data using DOM API

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const API_URL =
   "https://api.github.com/repos/sahanr/street-fighter/contents/fighters.json";
 
+const rootElement = document.getElementById("root");
+rootElement.innerText = "Loading...";
+
 fetch(API_URL)
   .then((response) => response.json())
   .then((file) => {
     const fighters = JSON.parse(atob(file.content));
     const names = fighters.map((it) => it.name).join("\n");
-    console.log(names);
+    rootElement.innerText = names;
   });
