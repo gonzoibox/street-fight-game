de195aaed44d8a07f9616158c3f29f2d4af81591
gonzoibox <gonzoibox@gmail.com>
2021-01-17 16:11:41 +0200

    add .then method to get response body

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const API_URL =
   "https://api.github.com/repos/sahanr/street-fighter/contents/fighters.json";
+
+const responsePromise = fetch(API_URL);
+responsePromise.then((response) => {
+  console.log(response);
+});
